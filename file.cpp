01ddbcdd12026c2231a9ebc13192c36e695065c9
Mansi Saini <mansisaini848@gmail.com>
2019-03-26 10:29:56 +0530

    adding main function

--- file.cpp
+++ file.cpp
@@ -9,4 +9,28 @@ class operations
 	float multiply(x,y);
 	float divide(x,y);
 };
+int main()
+{
+	int op;
+	cout<<"Enter operation"(+ - * and %);
+	cin>>op;
+	if(op == +)
+	{add(6,7);
+		break;
+	}
+	if(op == -)
+	{ sub(8,5);
+		break;
+	}
+	if(op == *)
+	{
+		multiply(7,4);
+		break;
+	}
+	if(op == %)
+	{divide(6,3);
+		break;
+	}
+}
+
 
